6b84821458f429101e243e4a555093761ad05546
mertdogramaci <mertdogramaci@gmail.com>
2022-11-12 13:14:51 +0300

    home page gui has been done

--- src/view/MessageBoxGUI.java
+++ src/view/MessageBoxGUI.java
@@ -7,19 +7,24 @@ public class MessageBoxGUI {
 
 
     public MessageBoxGUI() {
+        HomePage();
+    }
+
+    private void HomePage() {
         JFrame frame = new JFrame("Main Page");
         frame.setSize(400, 300);
 
         JLabel label = new JLabel("Welcome to MessageBox");
         label.setBounds(50, 40, 300, 30);
+        label.setFont(new Font("PMN Caecilia Sans Head Black", Font.BOLD, 24));
         frame.add(label);
 
         JButton accessButton = new JButton("Access");
-        accessButton.setBounds(150, 130, 100, 30);
+        accessButton.setBounds(140, 130, 100, 30);
         frame.add(accessButton);
 
         JButton leaveMessageButton = new JButton("Leave a message");
-        leaveMessageButton.setBounds(130, 175, 140, 45);
+        leaveMessageButton.setBounds(120, 175, 140, 45);
         frame.add(leaveMessageButton);
 
         frame.setLayout(null);
