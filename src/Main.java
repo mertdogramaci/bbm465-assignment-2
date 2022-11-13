b500040552c6e1792762894d586338659f7ca929
mertdogramaci <mertdogramaci@gmail.com>
2022-11-13 14:53:35 +0300

    user related codes have been added

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
+import controller.UserController;
 import view.MessageBoxGUI;
 
 public class Main {
     public static void main(String[] args) {
+        UserController userController = new UserController();
+        System.out.println(userController.getAllUsers());
         MessageBoxGUI messageBoxGUI = new MessageBoxGUI();
     }
 }
--- src/controller/UserController.java
+++ src/controller/UserController.java
@@ -2,6 +2,32 @@ package controller;
 
 import model.User;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserController {
-    private User[] users;
+    private final List<User> users;
+
+    public UserController() {
+        users = new ArrayList<>();
+
+        File userFile = new File("users.data");
+        try {
+            BufferedReader userText = new BufferedReader(new FileReader(userFile));
+            String line = userText.readLine();
+            while (line != null) {
+                String[] items = line.split("-");
+                User user = new User(items[0], items[1]);
+                users.add(user);
+                line = userText.readLine();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<User> getAllUsers() {
+        return users;
+    }
 }
