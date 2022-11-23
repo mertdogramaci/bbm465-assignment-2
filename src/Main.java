e4c04c9b9522ecd88ae2ff7c4054c45a2394268b
Mustafa Çağrı Korkmaz <m.cagrikorkmaz@outlook.com>
2022-11-23 19:31:25 +0300

    Project has been completed

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
 import controller.MessageController;
+import controller.UserController;
 import view.HomePage;
 
 public class Main {
     public static void main(String[] args) {
-        MessageController messageController = new MessageController();
+        UserController userController = new UserController();
+        MessageController messageController = new MessageController(userController);
         HomePage homePage = new HomePage(messageController);
     }
 }
